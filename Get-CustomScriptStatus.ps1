299f7f6688e913c15d64c3a96e3a5b2dc273974f
Mike Lee <62190454+mikelee1313@users.noreply.github.com>
2024-05-23 17:47:34 -0400

    Update Get-CustomScriptStatus.ps1

--- Get-CustomScriptStatus.ps1
+++ Get-CustomScriptStatus.ps1
@@ -5,10 +5,7 @@
 .DESCRIPTION
         This script connects to SharePoint Online and retrieves information about the Custom Script settings for all sites. 
         It checks whether Custom Script is enabled or disabled on each site and generates a CSV report with the results.
-
-.PARAMETER Tenant
-        Specifies the name of the SharePoint Online tenant.
-
+        
 .NOTES
         File Name: Get-CustomScriptStatus.ps1
         Author: Mike Lee
